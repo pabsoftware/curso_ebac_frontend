995da36d749cae3a2e01d6cecfc806cf992cfe51
pabsoftware <pabsoftware@outlook.com>
2023-07-19 20:53:20 -0300

    melhoria da marcação tarefa concluida

--- statics/js/main.js
+++ statics/js/main.js
@@ -12,29 +12,14 @@ $(document).ready(function(){
         const novo_item_li = $('<li style="diplay:none;"> ' + tarefa + '</li> ');
         $(novo_item_li).appendTo('ul');
 
-        const descendentes = document.querySelectorAll("ul li");
+        const descendentes =$(".tarefas-list li");
 
         for (var i = 0; i < descendentes.length; i++) {
             descendentes[i].addEventListener("click", function (e) {
-            
-            //Adicionei a opção de marcar ou desmarcar tarefa concluida para o caso de click acidental ter como reverter
-            if (e.target.style.textDecoration == "line-through"){
-                e.target.style.cssText = "null"
-            }else {
-                e.target.style.textDecoration = "line-through"
-            }
-
-            
+            e.target.style.textDecoration = "line-through"
             
         })
         }
-
-        $('ul li').click(function(){
-           
-           
-       
-        })
-       
         
     })
 
