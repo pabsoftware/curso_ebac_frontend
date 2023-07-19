d305f0a2fd5f9931abd7b4f792caa970fd05402c
pabsoftware <pabsoftware@outlook.com>
2023-07-19 20:35:20 -0300

    Add line-through ao <li>

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,9 +1,35 @@
 
-$('form').on('submit', function(e){
-    e.preventDefault();
+$(document).ready(function(){
+    
+  
 
-    const tarefa = $('#nova-tarefa').val()
-    //Concatenei com o + pois a form ${terefa} não esta funcionando aqui pra mim
-    const novo_item_li = $('<li style="diplay:none;"> ' + tarefa + '</li> ');
-    $(novo_item_li).appendTo('ul');
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const tarefa = $('#nova-tarefa').val()
+        //Concatenei com o + pois a form ${terefa} não esta funcionando aqui pra mim
+        const novo_item_li = $('<li style="diplay:none;"> ' + tarefa + '</li> ');
+        $(novo_item_li).appendTo('ul');
+
+        const descendentes = document.querySelectorAll("ul li");
+
+        for (var i = 0; i < descendentes.length; i++) {
+            descendentes[i].addEventListener("click", function (e) {
+            
+            e.target.style.textDecoration = "line-through"
+            
+        })
+        }
+
+        $('ul li').click(function(){
+           
+           
+       
+        })
+       
+        
+    })
+
+
 })
