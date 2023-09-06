8cc17ab1a979c715edba715fced0a4dcc3306549
pabsoftware <pabsoftware@outlook.com>
2023-09-06 16:17:34 -0300

    Concluida a automatização com gulp

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,5 @@
-//teste automação
+
+var css = "#product-item{border: 2px solid}"
+const header = document.getElementById('header');
+const card = document.getElementById('');
+header.style.boxShadow = "0px 8px  green";
