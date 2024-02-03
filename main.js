bd84946593f7b7528f0a7c14cd92133a2558cff7
pabsoftware <pabsoftware@outlook.com>
2024-02-03 15:11:46 -0300

    Tratando exceções o Fetch

--- main.js
+++ main.js
@@ -6,21 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const follower      = document.querySelector('#follower');
     const link          = document.querySelector('#link');
     const avatar        = document.querySelector('#avatar');
-
-    fetch ('https://api.github.com/users/pabsoftware')
-        .then(function(resposta) {
-            return resposta.json();
-            
-        })
-        .then(function(json){
-            
-            name.innerText          = json.name;
-            username.innerText      = json.login;
-            repositorios.innerText  = json.public_repos;
-            followers.innerText     = json.followers;
-            follower.innerText      = json.following;
-            link.href               = json.html_url;
-            avatar.src              = json.avatar_url;
-            
-        })
+    const box_error     = document.querySelector('#error');
+    const andpoint      = 'https://api.github.com/users/pabsoftware';
+ 
+        fetch (andpoint)
+            .then(function(resposta) {
+                    //Aqui fiz ele cair no catch, mesmo tendo conexão com o servidor, porem sem sucesso na obtenção dos dados.
+                    if (!resposta.ok) {
+                        throw Error(resposta.statusTex);
+                    }
+                    
+                return resposta.json(); 
+            }) 
+            .then(function(json){
+                
+                name.innerText          = json.name;
+                username.innerText      = json.login;
+                repositorios.innerText  = json.public_repos;
+                followers.innerText     = json.followers;
+                follower.innerText      = json.following;
+                link.href               = json.html_url;
+                avatar.src              = json.avatar_url;
+            })
+            //O then já possue função do try e havendo erro com conexão ou outro, cairá no catch
+            .catch(function(erro){
+                box_error.innerText = "Houve um erro no carregamento dos dados. Favor tente mais tarde";
+                box_error.style.display = "block";
+                
+            })
+   
 })
