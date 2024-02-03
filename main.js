1fd9664523fb98de53bddfe206a8c0c7c87aab33
pabsoftware <pabsoftware@outlook.com>
2024-02-03 10:19:34 -0300

    finalizando exercicio M23

---
+++ main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const name          = document.querySelector('#name')
+    const username      = document.querySelector('#username');
+    const repositorios  = document.querySelector('#repos');
+    const followers     = document.querySelector('#followers');
+    const follower      = document.querySelector('#follower');
+    const link          = document.querySelector('#link');
+    const avatar        = document.querySelector('#avatar');
+
+    fetch ('https://api.github.com/users/pabsoftware')
+        .then(function(resposta) {
+            return resposta.json();
+            
+        })
+        .then(function(json){
+            console.log(json);
+            name.innerText = json.name;
+            username.innerText = json.login;
+            repositorios.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            follower.innerText = json.following;
+            link.href = json.html_url;
+            avatar.src = json.avatar_url;
+            
+        })
+})
