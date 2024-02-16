84820a69433a47f0ff5759f6044d35beaa652be8
pabsoftware <pabsoftware@outlook.com>
2024-02-16 09:19:04 -0300

    Criado array de objeto

---
+++ main.js
@@ -0,0 +1,30 @@
+//Exercício modulo 25
+
+const arrayAlunos = [
+    {
+        "nome": "Paulo",
+        "nota": 8
+    },
+    {
+        "nome": "João",
+        "nota": 10
+    },
+    {
+        "nome": "Pedro",
+        "nota": 5
+    },
+    {
+        "nome": "José",
+        "nota": 6
+    },
+    {
+        "nome": "Tiago",
+        "nota": 7
+    },
+    {
+        "nome": "Joel",
+        "nota": 4
+    }
+
+];
+
